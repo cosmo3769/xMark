e55356cea267e1ff52e63bebb8c2c55e1e33b3af
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-09-20 17:17:16 +0530

    update

--- static/styles/func.js
+++ static/styles/func.js
@@ -1,3 +1,8 @@
 $("#wrapper").click(function() {
     $(".icon").toggleClass("close");
 })
+
+$("#hype").hover(function(){
+    $("hype-all").slideUp(1000);
+     $("#hype-text").fadeToggle(1000);
+});
