d1f6f1f339675bf6b372cbb03e5d42ff96c450d8
Ayush <ayushmishra2854@gmail.com>
2020-09-29 07:49:08 +0530

    base done

--- static/styles/func.js
+++ static/styles/func.js
@@ -1,3 +1,8 @@
+const mq= window.matchMedia("(max-width: 992px)");
+
 $("#wrapper").click(function() {
     $(".icon").toggleClass("close");
+    if(mq.matches){
+    $(".curtain").toggle(500);
+    }
 })
